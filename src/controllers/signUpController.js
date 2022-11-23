7765b018ca77cb260e7ddbe686b2d7085747c4f5
caiohbe <caiohbe@DESKTOP-CK15AF0.localdomain>
2022-11-23 17:31:41 -0300

    feat: post /sign-up controller & middleware

--- src/controllers/signUpController.js
+++ src/controllers/signUpController.js
@@ -1,8 +1,23 @@
 import db from "../database/db.js"
+import bcrypt from "bcrypt"
 
 export async function postSignUp (req, res) {
-    try {
+    const { email, name } = req.body
+    const password = bcrypt.hashSync(req.body.password, 10)
 
+    const newUser = {
+        email,
+        name,
+        password
+    }
+
+    try {
+        if (await db.collection("signUps").findOne({email})) {
+            res.status(409).send("Email já cadastrado.")
+        } else {
+            await db.collection("signUps").insertOne(newUser)
+            res.sendStatus(200)
+        }
     } catch (err) {
         res.status(500).send(err.message)
     }
--- src/middlewares/signUpMiddleware.js
+++ src/middlewares/signUpMiddleware.js
@@ -1,4 +1,26 @@
+import joi from "joi";
+
+const signUpSchema = joi.object({
+    name: joi.string().required(),
+    adress: joi.string().required(),
+    email: joi.string().required(),
+    password: joi.string().required(),
+    confirmPassword: joi.string().required()
+})
+
 export function validateSignUp (req, res, next) {
-    
+    const validation = signUpSchema.validate(req.body, { abortEarly: false })
+
+    if (validation.error) {
+        const errors = validation.error.details.map((detail) => detail.message)
+        res.status(422).send(errors)
+        return
+    }
+
+    if (req.body.password !== req.body.confirmPassword) {
+        res.status(409).send("Senhas precisam ser idênticas.")
+        return
+    }
+
     next()
 }
