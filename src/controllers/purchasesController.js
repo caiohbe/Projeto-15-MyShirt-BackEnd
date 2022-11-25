1d404a6a2995c8e8a3a0c8608ef978b66394484e
ana-lapas <anapaulalapas@gmail.com>
2022-11-24 23:12:25 -0400

    Purchase history, verify api

--- src/controllers/purchasesController.js
+++ src/controllers/purchasesController.js
@@ -1,15 +1,19 @@
 import db from "../database/db.js"
 
-export async function getPurchases (req, res) {
+export async function getPurchases(req, res) {
     try {
-        
+
     } catch (err) {
         res.status(500).send(err.message)
     }
 }
 
-export async function postPurchases (req, res) {
+export async function postPurchases(req, res) {
+    const newPurchase = res.locals.purchase;
+    /*VERIFICAR COMO FICA A VALIDAÇÃO SE COMPRAR MAIS DE UM OBJETO*/
     try {
+        await db.collections("purchases").insertOne(newPurchase);
+        res.sendStatus(201);
 
     } catch (err) {
         res.status(500).send(err.message)
--- src/middlewares/purchaseMiddleware.js
+++ src/middlewares/purchaseMiddleware.js
@@ -1,4 +1,26 @@
-export function validatePurchase (req, res, next) {
+import joi from "joi";
 
+const purchaseSchema = joi.object({
+    idProduct: joi().string().required(),
+    qtt: joi().number().required()
+})
+/*TODO: VERIFICAR COMO FAZER A VALIDAÇÃO DE UM OBJETO QUE TARÁ VÁRIOS OBJETOS MENORES*/
+export function validatePurchase(req, res, next) {
+    const { idProduct, qtt } = req.body;
+    const user = res.locals.user;
+
+    const newPurchase = {
+        idProduct,
+        qtt,
+    };
+
+    const { error } = purchaseSchema.validate(newPurchase, { abortEarly: false });
+
+    if (error) {
+        const errors = error.details.map((detail) => detail.message);
+        return res.status(400).send(errors);
+    }
+
+    res.locals.purchase = purchase;
     next()
 }
